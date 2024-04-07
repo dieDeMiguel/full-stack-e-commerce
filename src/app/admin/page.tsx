73228505fa8123da930b363b8f09baa58cdcca08
Progress with the component <diedemiguel@gmail.com>
2024-04-07 20:54:41 +0200

    Update dependencies and add sales data to admin dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,19 +5,49 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import db from "@/db/db";
 
-export default function AdminDashboard() {
+async function getSalesData() {
+  const data = await db.order.aggregate({
+    _sum: { pricePaidInCents: true },
+    _count: true,
+  });
+
+  return {
+    amount: (data._sum.pricePaidInCents || 0) / 100,
+    numberOfSales: data._count,
+  };
+}
+
+export default async function AdminDashboard() {
+  const salesData = await getSalesData();
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <Card>
-        <CardHeader>
-          <CardTitle>Sales</CardTitle>
-          <CardDescription>desc</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Text</p>
-        </CardContent>
-      </Card>
+      <DashboardCard
+        title="Sales"
+        subtitle={salesData.numberOfSales.toString()}
+        body={salesData.amount.toString()}
+      />
     </div>
+  );
+}
+
+type DashboardCardProps = {
+  title: string;
+  subtitle: string;
+  body: string;
+};
+
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
   );
 }
