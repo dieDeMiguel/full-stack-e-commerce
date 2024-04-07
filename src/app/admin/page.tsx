ac830826d1b39480b2e920a997ff79e9b2ad425c
Progress with the component <diedemiguel@gmail.com>
2024-04-07 20:59:27 +0200

    Refactor admin dashboard to use formatted sales data

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -6,6 +6,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import db from "@/db/db";
+import { formatCurrency, formatNumber } from "@/lib/formaters";
 
 async function getSalesData() {
   const data = await db.order.aggregate({
@@ -25,8 +26,8 @@ export default async function AdminDashboard() {
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <DashboardCard
         title="Sales"
-        subtitle={salesData.numberOfSales.toString()}
-        body={salesData.amount.toString()}
+        subtitle={formatNumber(salesData.numberOfSales)}
+        body={formatCurrency(salesData.amount)}
       />
     </div>
   );
