d0c32b2e41aad5e318327b6f798aafca69bde349
Progress with the component <diedemiguel@gmail.com>
2024-04-09 08:08:55 +0200

    Update dependencies and add @radix-ui/react-label and @radix-ui/react-slot packages

---
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { formatEuroCurrency } from "@/lib/formaters";
+import { Label } from "@radix-ui/react-label";
+import { useState } from "react";
+
+export default function ProductForm() {
+  const [priceInCents, setPriceInCents] = useState<number | undefined>();
+
+  return (
+    <form action={(value) => console.log(value)} className="space-y-8">
+      <div className="space-y-2">
+        <Label htmlFor="name">Name</Label>
+        <Input type="text" id="name" name="name" required />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="priceInCents">Price in Cents</Label>
+        <Input
+          type="text"
+          id="priceInCents"
+          name="priceInCents"
+          required
+          onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
+        />
+        <div className="text-muted-foreground">
+          {formatEuroCurrency((priceInCents || 0) / 100)}
+        </div>
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="description">Description</Label>
+        <Textarea id="description" name="description" required />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="file">File</Label>
+        <Input type="file" id="file" name="file" required />
+      </div>
+      <Button type="submit">Save</Button>
+    </form>
+  );
+}
