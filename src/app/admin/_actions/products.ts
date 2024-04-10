930707337739fdb3955f2a1feec610f29f740820
Progress with the component <diedemiguel@gmail.com>
2024-04-10 09:47:49 +0200

    Update isAvailableForPurchase to false when adding a product

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -38,7 +38,7 @@ export async function addProduct(prevState: unknown, formData: FormData) {
 
   await db.product.create({
     data: {
-      isAvailableForPurchase: true,
+      isAvailableForPurchase: false,
       name: data.name,
       description: data.description,
       priceInCents: data.priceInCents,
--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -4,20 +4,29 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { formatEuroCurrency } from "@/lib/formatters";
+import { Product } from "@prisma/client";
 import { Label } from "@radix-ui/react-label";
 import { useState } from "react";
 import { useFormState, useFormStatus } from "react-dom";
 import { addProduct } from "../../_actions/products";
 
-export default function ProductForm() {
-  const [priceInCents, setPriceInCents] = useState<number>();
+export default function ProductForm({ product }: { product?: Product | null }) {
   const [error, action] = useFormState(addProduct, {});
+  const [priceInCents, setPriceInCents] = useState<number | undefined>(
+    product?.priceInCents
+  );
 
   return (
     <form action={action} className="space-y-8">
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
-        <Input type="text" id="name" name="name" required />
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          required
+          defaultValue={product?.name || ""}
+        />
         {error.name && <div className="text-destructive">{error.name}</div>}
       </div>
       <div className="space-y-2">
@@ -39,19 +48,29 @@ export default function ProductForm() {
       </div>
       <div className="space-y-2">
         <Label htmlFor="description">Description</Label>
-        <Textarea id="description" name="description" required />
+        <Textarea
+          id="description"
+          name="description"
+          required
+          defaultValue={product?.description || ""}
+        />
         {error.description && (
           <div className="text-destructive">{error.description}</div>
         )}
       </div>
       <div className="space-y-2">
         <Label htmlFor="file">File</Label>
-        <Input type="file" id="file" name="file" required />
+        <Input type="file" id="file" name="file" required={product === null} />
         {error.file && <div className="text-destructive">{error.file}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="image">Image</Label>
-        <Input type="file" id="image" name="image" required />
+        <Input
+          type="file"
+          id="image"
+          name="image"
+          required={product === null}
+        />
         {error.image && <div className="text-destructive">{error.image}</div>}
       </div>
       <SubmitButton />
