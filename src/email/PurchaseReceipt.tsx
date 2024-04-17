22f3342e80388bce131b991d6959b2e8bcfb4eb7
Progress with the component <diedemiguel@gmail.com>
2024-04-17 19:38:58 +0200

    Add @react-email/components dependency and email script to package.json

--- src/email/PurchaseReceipt.tsx
+++ src/email/PurchaseReceipt.tsx
@@ -23,8 +23,7 @@ PurchaseReceiptEmail.PreviewProps = {
   product: {
     name: "Product name",
     description: "Some description",
-    imagePath:
-      "/products/5aba7442-e4a5-4d2e-bfa7-5bd358cdad64-02 - What Is Next.js.jpg",
+    imagePath: "/products/1beef135-7dbd-4a46-be97-8e5cfd9e5457-IMG_2465.jpg",
   },
   order: {
     id: crypto.randomUUID(),
